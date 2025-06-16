004e8a9a5e6f25d8b9ea511b82a1a266d507ad19
Sanu700 <156925758+Sanu700@users.noreply.github.com>
2025-06-16 18:29:42 +0530

    Update ScrollToTop.tsx

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,22 +1,21 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-<<<<<<< Anirudh
+
 const ScrollToTop = () => {
-=======
+
 export default function ScrollToTop() {
->>>>>>> main
-  const { pathname } = useLocation();
+
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
   return null;
-<<<<<<< Anirudh
+
 };
 
 export default ScrollToTop; 
-=======
+
 } 
->>>>>>> main
+
